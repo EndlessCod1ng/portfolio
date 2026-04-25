ddabc8d61c1b7bc7a758b6c04c62b17a9ca3e6a9
Endless Coding <endlesscoding@yahoo.com>
2026-04-25 18:07:08 +0300

    ch w/Stck

--- src/widgets/Stack/ui/Stack.tsx
+++ src/widgets/Stack/ui/Stack.tsx
@@ -7,6 +7,26 @@ import F from "@/shared/assets/images/banner/f.png"
 import Ps from "@/shared/assets/images/banner/ps.png"
 import { AppContainer } from "@/shared/ui/AppContainer/AppContainer"
 import { AppTitle } from "@/shared/ui/AppTitle/AppTitle"
+import { AppIcon } from "@/shared/ui/AppIcon/AppIcon"
+
+import type { FC, SVGProps } from "react";
+
+type SvgComponent = FC<SVGProps<SVGSVGElement>>;
+
+const svgModules: Record<string, { default: SvgComponent }> = import.meta.glob(
+  "@/shared/assets/images/stack/*.svg",
+  { query: '?react', eager: true }
+);
+
+const BannerIcons: Record<string, SvgComponent> = Object.entries(svgModules).reduce(
+  (acc, [path, module]) => {
+    const key = path.split('/').pop()?.replace('.svg', '') || path;
+    acc[key] = module.default;
+    return acc;
+  },
+  {} as Record<string, SvgComponent>
+);
+
 interface StackProps {
   className?: string
 }
@@ -16,9 +36,25 @@ const images = [
   { text: "Illustrator", img: Ai },
   { text: "Figma", img: F }
 ]
+
+// const icons: Record<string, { default: string }> = import.meta.glob(
+//   "@/shared/assets/images/stack/*.svg",
+//   { eager: true }
+// );
+
+// const stackIcons = Object.entries(icons).map(([path, module]) => {
+//   const fileName = path.split('/').pop()?.replace('.svg', '');
+//   return {
+//     name: fileName,
+//     url: module.default
+//   };
+// });
+
+
 export const Stack = (
   { className }: StackProps
 ) => {
+
   return (
     <AppContainer className={cn(s.stack, className)}>
       <AppTitle title={"Stack"} className={s.title} />
@@ -27,6 +63,16 @@ export const Stack = (
           <AppTitle TagName="h4" title={image.text} />
           <img className={s.icon} src={image.img} />
         </div>))}
+
+      </div>
+      <div className={s.icons}>
+        {Object.entries(BannerIcons).map(([name, Icon]) => (
+          <AppIcon
+            key={name}
+            Svg={Icon}
+            size="l"
+          />
+        ))}
       </div>
     </AppContainer>
   )
